b63ccab8e0649be9af29f8f9abbaf15145c4f919
JiangYeLanAn <598054078@qq.com>
2026-05-31 03:11:43 +0800

    fix: update to new fish icon (icon_v100), bump v0.1.4

--- src/chatViewProvider.ts
+++ src/chatViewProvider.ts
@@ -442,7 +442,7 @@ export class ChatViewProvider implements vscode.WebviewViewProvider {
                     apiKeyStored: hasKey,
                     tuiVersion,
                     tuiConnected: this.tuiManager.connected,
-                    extVersion: '0.1.3',
+                    extVersion: '0.1.4',
                     nodeVersion: process.version,
                     vscodeVersion: vscode.version,
                 });
